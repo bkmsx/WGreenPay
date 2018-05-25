8231644feb26ba67b45970a3fa4e2af713d05c11
Bkmsx <tien@novum.capital>
2018-05-25 14:12:17 +0700

    add Google tracking tag

--- index-kr.php
+++ index-kr.php
@@ -24,6 +24,13 @@
 <script type="text/javascript" src="js/jquery.flipcountdown.js"></script>
 <script src="https://use.typekit.net/bkt6ydm.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5X2FMMG');</script>
+<!-- End Google Tag Manager -->
 
 <style>
 
@@ -118,6 +125,10 @@ function closeSidebar(e) {
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5X2FMMG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!------------ Navigation start ------------>
 <div id="header">
