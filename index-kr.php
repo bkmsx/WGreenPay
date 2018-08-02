67f1993fb2662f21bf33e5558082287d76d1656c
jonasnovum <39366813+jonasnovum@users.noreply.github.com>
2018-08-02 11:34:06 +0800

    Update index-kr.php

--- index-kr.php
+++ index-kr.php
@@ -1124,6 +1124,7 @@ Private Sale
 <div class="team-title">
 <h3>Jeremy Khoo</h3>
 <h4>W재단 Business Development Adviser</h4>
+	<p>(Group CEO iFashion Group, Crypto Fund Partner, Blockchain Entrepreneur)</p>
 </div>
 </div>
 <div class="col-md-3 col-sm-6 v-pad">
@@ -1132,6 +1133,7 @@ Private Sale
 <div class="team-title">
 <h3>Irwin Chee</h3>
 <h4>W재단 Business Development Adviser</h4>
+	<p>(Venture Capital Partner, Crypto Fund Partner, Blockchain Consultant)</p>
 </div>
 </div>
 <div class="col-md-3 col-sm-6 v-pad">
